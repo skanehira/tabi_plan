7daf0fff5d799a2395415d8133cdec1a9a8c0c30
skanehira <sho19921005@gmail.com>
2023-05-14 19:07:55 +0900

    feat: support graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
 use axum::{routing::get, Router};
+use tokio::signal;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!\n" }));
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(target_os = "linux")]
+    let host = std::env::var("HOSTNAME")?;
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    #[cfg(target_os = "macos")]
+    let host = {
+        let output = std::process::Command::new("hostname").output()?.stdout;
+        String::from_utf8(output)?.trim().to_string()
+    };
+
+    let app = Router::new().route(
+        "/",
+        get(move || async move { format!("Hello, {}!\n", host) }),
+    );
+
+    eprintln!("server start");
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
