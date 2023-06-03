925180ec33cda46e9e375b7fa8335a68f4cfe79c
Okina Asami <asami.okina@craftsman-software.com>
2023-06-03 14:04:06 +0900

    updates after review

--- src/routes.rs
+++ src/routes.rs
@@ -1,59 +1,38 @@
 use axum::response::Json;
-use serde::Serialize;
-use serde_json::{json, Value};
+use serde::{Deserialize, Serialize};
 
-pub async fn handler(body_json: Json<Value>) -> Json<Value> {
-    let area = body_json.0.get("area").unwrap().as_str().unwrap();
-    let candidate_count = body_json
-        .0
-        .get("candidate_count")
-        .unwrap()
-        .as_str()
-        .unwrap();
-    let travel_mode = body_json.0.get("travel_mode").unwrap().as_str().unwrap();
-    let _input = Input {
-        area: area.to_string(),
-        candidate_count: candidate_count.parse::<u8>().unwrap(),
-        travel_mode: match travel_mode {
-            "walking" => TravelMode::Walking,
-            "driving" => TravelMode::Driving,
-            _ => panic!("invalid travel_mode"),
-        },
-    };
-    let response = dummy().await.unwrap();
-    Json(json!({ "response": response }))
-}
-
-async fn dummy() -> Result<Output, anyhow::Error> {
-    let dummy_response = Output {
+pub async fn handler(_input: Json<Input>) -> Json<Output> {
+    let resp = Output {
         places: vec![Place {
-            place: "dummy".to_string(),
+            place: "< place >".to_string(),
         }],
-        google_map_url: "dummy".to_string(),
+        google_map_url: "< url >".to_string(),
     };
-    Ok(dummy_response)
+    Json(resp)
 }
 
-#[derive(Debug, Serialize)]
-struct Input {
-    area: String,
-    candidate_count: u8,
-    travel_mode: TravelMode,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Input {
+    pub area: String,
+    pub candidate_count: u8,
+    pub travel_mode: TravelMode,
 }
 
-#[derive(Debug, Serialize)]
-struct Output {
-    places: Vec<Place>,
-    google_map_url: String,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Output {
+    pub places: Vec<Place>,
+    pub google_map_url: String,
 }
 
-#[derive(Debug, Serialize)]
-struct Place {
-    place: String,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Place {
+    pub place: String,
 }
 
-#[derive(Debug, Serialize)]
-enum TravelMode {
+#[derive(Debug, Deserialize, Serialize)]
+pub enum TravelMode {
+    #[serde(rename = "walking")]
     Walking,
+    #[serde(rename = "bicycling")]
     Driving,
 }
