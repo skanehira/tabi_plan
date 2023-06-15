b617c0c8d51fc55ca26bb4e3cee68474454f648e
skanehira <sho19921005@gmail.com>
2023-06-15 21:17:52 +0900

    chore: add test

--- src/routes.rs
+++ src/routes.rs
@@ -46,3 +46,52 @@ pub async fn get_routes(
     };
     Ok(Json(resp))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{config, input::routes::TravelMode};
+    use axum::{
+        body::Body,
+        http::{self, Request, StatusCode},
+        routing::{post, Router},
+    };
+    use chatgpt::prelude::ChatGPT;
+    use clap::Parser as _;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn test_get_routes() -> anyhow::Result<()> {
+        let config = config::AppConfig::try_parse()?;
+        let token = config.open_chat.open_chat_api_key.clone();
+
+        let state = config::AppState {
+            config,
+            chat_gpt_client: ChatGPT::new(token)?,
+            google_map_client: reqwest::Client::new(),
+        };
+        let state = Arc::new(state);
+        let app = Router::new()
+            .route("/routes", post(get_routes))
+            .with_state(Arc::clone(&state));
+
+        let input = Input {
+            origin: "大阪城".into(),
+            destination: "USJ".into(),
+            waypoints: vec!["道頓堀".into(), "通天閣".into(), "天保山".into()],
+            travel_mode: TravelMode::Driving,
+        };
+        let json = serde_json::to_vec(&input)?;
+        let body = Body::from(json);
+
+        let req = Request::builder()
+            .method(http::Method::POST)
+            .uri("/routes")
+            .body(body)?;
+
+        let resp = app.oneshot(req).await?;
+        assert_eq!(resp.status(), StatusCode::OK);
+
+        Ok(())
+    }
+}
